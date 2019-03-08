f24ff48fc3a221e7400b70e897ad3494f5c3a759
Mark <sanch0220@gmail.com>
2019-03-07 22:42:24 -0600

    get android manifest back

---
+++ TripLog/Views/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using TripLog.Models;
+using System.Collections.ObjectModel;
+using Xamarin.Forms.Maps;
+
+
+namespace TripLog.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var items = new List<TripLogEntry>
+            {
+                new TripLogEntry
+                {
+                    Title = "Washington Monument",
+                    Notes = "Amazing!",
+                    Rating = 3,
+                    Date = new DateTime(2017, 2, 5),
+                    Latitude = 38.8895,
+                    Longitude = -77.0352
+                },
+                new TripLogEntry
+                {
+                    Title = "Statute of Liberty",
+                    Notes = "Inspiring!",
+                    Rating = 4,
+                    Date = new DateTime(2017, 4, 13),
+                    Latitude = 40.6892,
+                    Longitude = -74.0444
+                },
+                new TripLogEntry
+                {
+                    Title = "Golden Gate",
+                    Notes = "Foggy!",
+                    Rating = 5,
+                    Date = new DateTime(2017, 4, 26),
+                    Latitude = 37.8268,
+                    Longitude = -122.4798
+                }
+            };
+            trips.ItemsSource = items;
+
+        }
+
+        void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewEntryPage());
+
+        }
+
+        async void Trips_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var trip = (TripLogEntry)e.Item;
+            await Navigation.PushAsync(new DetailPage(trip));
+
+            //Clear selection
+            trips.SelectedItem = null;
+        }
+    }
+}
